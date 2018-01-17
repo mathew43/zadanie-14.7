5ccd47ff32447a16461ecf626f7b1694b46dfce1
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2018-01-17 09:25:36 +0100

    zadanie 14.7

--- components/App.js
+++ components/App.js
@@ -1,31 +1,29 @@
 var contacts = [
-{
-	id: 1,
-	firstName: 'Jan',
-	lastName: 'Nowak',
-	email: 'jan.nowak@example.com',
-},
-{
-	id: 2,
-	firstName: 'Adam',
-	lastName: 'Kowalski',
-	email: 'adam.kowalski@example.com',
-},
-{
-	id: 3,
-	firstName: 'Zbigniew',
-	lastName: 'Koziol',
-	email: 'zbigniew.koziol@example.com',
-}
-
+  {
+    id: 1,
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    email: 'jan.nowak@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'Adam',
+    lastName: 'Kowalski',
+    email: 'adam.kowalski@example.com',
+  },
+  {
+    id: 3,
+    firstName: 'Zbigniew',
+    lastName: 'Koziol',
+    email: 'zbigniew.koziol@example.com',
+  }
 ];
 
 var contactForm = {
-	firstName: '',
-	lastName: '',
-	email: ''
+  firstName: '',
+  lastName: '',
+  email: ''
 };
-
 
 var App = React.createClass({
     render: function(){
@@ -34,4 +32,4 @@ var App = React.createClass({
             <Contacts items={contacts}/>
             </div>);
     },
-});
+})
